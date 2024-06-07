e99d323827b1d20289c71f77441656d747efe47c
Dielli Demjaha <diellidemjaha@gmail.com>
2024-06-07 09:03:53 +0200

    Update AboutUs.jsx

--- react-frontend/src/AboutUs.jsx
+++ react-frontend/src/AboutUs.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Image from "./assets/321.jpg";
-// import {Link} from "react-router-dom"
 
 const AboutUs = () => {
   return (
@@ -12,25 +11,15 @@ const AboutUs = () => {
           Wherever you go, no matter what the weather, always bring your own sunshine.
           </p>
           <h5 className="card-title">Anthony J. D'Angelo</h5>
-          {/* <Link to="/cities" className="mt-3">
-            <button className="btn btn-primary">Check Weather</button>
-          </Link> */}
         </div>
         </div>
     <div className=" p-4 bg-dark text-light">
-      {/* <div className="card"> */}
-        {/* <div className="card-body"> */}
-            {/* <div className='col-md-6'> */}
-
           <h5>About KS Drive</h5>
           <p style={{ width: '50%'}}>
             At KS Drive, we believe in fostering safe driving practices through informed decision-making. Our mission is to provide real-time weather data to empower drivers across Kosovo. Explore the weather dashboard, city-specific details, and safety tips to make every drive a safer journey.
           </p>
-          {/* Additional Sections Like Our Team, Mission Statement, Values, and Contact Information Can Be Added */}
             </div>
     </>
-    // </div>
-    // </div>
   );
 }
 
